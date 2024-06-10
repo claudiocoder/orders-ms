c5131abf47d5dba80ab512afaf9f58a5cce28179
claudiocoder <clauartur98@gmail.com>
2024-06-10 16:15:45 -0600

    :sparkles: - Connect orders controller to client gateway

---
+++ src/orders/orders.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class OrdersService {
+  create() {
+    return 'This action adds a new order';
+  }
+
+  findAll() {
+    return `This action returns all orders`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} order`;
+  }
+}
