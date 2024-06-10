19d2817ec66f2dc4525021e48c39a9d50fbe831f
claudiocoder <clauartur98@gmail.com>
2024-06-10 15:36:58 -0600

    :sparkles: - Update PORT envs controller

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Orders-Controller');
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(envs.port);
+  logger.log(`Server is running on ${envs.port}`);
 }
 bootstrap();
